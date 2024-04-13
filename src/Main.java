4cea34eb81b13534ca685d000d6d734f5decd5d3
Nick <neatomaru@gmail.com>
2024-04-13 04:36:39 +0300

    add scanner from console

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import Entity.Query;
 import services.QueryParser;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         var sqlQuery = "SELECT column1, column2 FROM table1 WHERE column3 = 5 GROUP BY column4 ORDER BY column2 LIMIT 10 OFFSET 20";
@@ -16,6 +18,16 @@ public class Main {
                 "LIMIT 10;";
         Query query2 = QueryParser.parse(sqlQuery);
         System.out.println(query2);
+
+
+        var scanner = new Scanner(System.in);
+        var inputQuery = scanner.nextLine();
+        while(!inputQuery.isEmpty()) {
+            System.out.println("----------------------------");
+            Query parsedQuery = QueryParser.parse(inputQuery);
+            System.out.println(parsedQuery);
+            inputQuery = scanner.nextLine();
+        }
 
         System.out.println("END");
     }
